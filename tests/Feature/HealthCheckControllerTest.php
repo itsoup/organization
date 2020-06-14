02678bad35e2fd11cbc81317d9d207d19f5f208e
Jose Postiga <jpostiga@infraspeak.com>
2020-06-14 10:04:56 +0100

    ci(PHPUnit): add APP_KEY config key

--- tests/Feature/HealthCheckControllerTest.php
+++ tests/Feature/HealthCheckControllerTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class HealthCheckControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function it_responds_to_health_check_requests(): void
     {
