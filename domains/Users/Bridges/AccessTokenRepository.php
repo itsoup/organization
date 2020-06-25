e55f50957d92b64175b3711b018611ec28398d88
José Postiga <jpostiga@infraspeak.com>
2020-06-25 18:20:13 +0100

    refactor(Users): add return type hint
    Signed-off-by: José Postiga <jpostiga@infraspeak.com>

--- domains/Users/Bridges/AccessTokenRepository.php
+++ domains/Users/Bridges/AccessTokenRepository.php
@@ -7,7 +7,7 @@ use League\OAuth2\Server\Entities\ClientEntityInterface;
 
 class AccessTokenRepository extends PassportAccessTokenRepository
 {
-    public function getNewToken(ClientEntityInterface $clientEntity, array $scopes, $userIdentifier = null)
+    public function getNewToken(ClientEntityInterface $clientEntity, array $scopes, $userIdentifier = null): UserAccessToken
     {
         return new UserAccessToken($userIdentifier, $scopes, $clientEntity);
     }
