f9a770f71e2d209e7e0a734dc13953977bd2d8bd
José Postiga <jpostiga@infraspeak.com>
2020-07-25 18:45:07 +0100

    chore(Exceptions): optimize logical condition
    Signed-off-by: José Postiga <jpostiga@infraspeak.com>

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,25 +8,6 @@ use Throwable;
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
-
-    /**
      * Report or log an exception.
      *
      * @param  \Throwable  $exception
@@ -37,25 +18,10 @@ class Handler extends ExceptionHandler
      */
     public function report(Throwable $exception): void
     {
-        if (app()->bound('sentry') && $this->shouldReport($exception)) {
-            app('sentry')->captureException($exception);
+        if ($this->shouldReport($exception) && app()->bound('sentry')) {
+            resolve('sentry')->captureException($exception);
         }
 
         parent::report($exception);
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $exception
-     *
-     * @throws \Throwable
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function render($request, Throwable $exception)
-    {
-        return parent::render($request, $exception);
     }
 }
