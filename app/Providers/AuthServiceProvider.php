05fb451dee26149f549000a9eca20b81fafc5b17
José Postiga <jpostiga@infraspeak.com>
2020-08-08 10:40:18 +0100

    feat(Auth): remove unused Passport routes
    Signed-off-by: José Postiga <jpostiga@infraspeak.com>

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
 use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
@@ -23,6 +24,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Passport::routes();
+        Passport::routes(
+            static function () {
+                Route::post('/token', [
+                    'uses' => 'AccessTokenController@issueToken',
+                    'as' => 'passport.token',
+                    'middleware' => 'throttle',
+                ]);
+            },
+        );
     }
 }
